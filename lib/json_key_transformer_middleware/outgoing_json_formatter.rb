2c6d8c199a35c672dd5a2dd866a9998b8543c883
Kevin Rood <kevin.rood@acceletroncorp.com>
2016-01-18 11:17:48 -0500

    Transforming outgoing JSON keys is now taken care of lazily

--- lib/json_key_transformer_middleware/outgoing_json_formatter.rb
+++ lib/json_key_transformer_middleware/outgoing_json_formatter.rb
@@ -11,17 +11,29 @@ module JsonKeyTransformerMiddleware
     def call(env)
       status, headers, body = @app.call(env)
 
-      new_body = body.each.map do |body_part|
-        begin
-          Oj.dump(
-            deep_transform_hash_keys(
-              Oj.load(body_part), :underscore_to_camel))
-        rescue
-          body_part
-        end
-      end
+      new_body = self.class.build_new_body(body)
 
       [status, headers, new_body]
+    end
+
+    private
+
+    def self.build_new_body(body)
+      Enumerator.new do |yielder|
+        body.each do |body_part|
+          yielder << transform_outgoing_body_part(body_part)
+        end
+      end
+    end
+
+    def self.transform_outgoing_body_part(body_part)
+      begin
+        Oj.dump(
+          deep_transform_hash_keys(
+            Oj.load(body_part), :underscore_to_camel))
+      rescue
+        body_part
+      end
     end
 
   end
