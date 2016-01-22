23deb11563542f916e5cb141d95f5e0329371124
Kevin Rood <kevin.rood@acceletroncorp.com>
2016-01-22 09:34:13 -0500

    Fix issue introduced by using class methods

--- lib/json_key_transformer_middleware/outgoing_json_formatter.rb
+++ lib/json_key_transformer_middleware/outgoing_json_formatter.rb
@@ -11,14 +11,14 @@ module JsonKeyTransformerMiddleware
     def call(env)
       status, headers, body = @app.call(env)
 
-      new_body = self.class.build_new_body(body)
+      new_body = build_new_body(body)
 
       [status, headers, new_body]
     end
 
     private
 
-    def self.build_new_body(body)
+    def build_new_body(body)
       Enumerator.new do |yielder|
         body.each do |body_part|
           yielder << transform_outgoing_body_part(body_part)
@@ -26,7 +26,7 @@ module JsonKeyTransformerMiddleware
       end
     end
 
-    def self.transform_outgoing_body_part(body_part)
+    def transform_outgoing_body_part(body_part)
       begin
         Oj.dump(
           deep_transform_hash_keys(
