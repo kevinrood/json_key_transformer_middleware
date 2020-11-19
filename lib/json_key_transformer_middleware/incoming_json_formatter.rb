ff4ea5fc4c7bd1c5585832e970db268298f47cf6
Lawrence Chou <lawrencechou1024@gmail.com>
2020-11-19 23:10:52 +0800

    Configurable to skip certain paths via `skip_paths`

--- lib/json_key_transformer_middleware/incoming_json_formatter.rb
+++ lib/json_key_transformer_middleware/incoming_json_formatter.rb
@@ -6,13 +6,15 @@ module JsonKeyTransformerMiddleware
   class IncomingJsonFormatter < Middleware
 
     def call(env)
-      object = Oj.load(env['rack.input'].read)
-      transformed_object = HashKeyTransformer.send(middleware_config.incoming_strategy, object, middleware_config.incoming_strategy_options)
-      result = Oj.dump(transformed_object, mode: :compat)
+      unless should_skip?(env)
+        object = Oj.load(env['rack.input'].read)
+        transformed_object = HashKeyTransformer.send(middleware_config.incoming_strategy, object, middleware_config.incoming_strategy_options)
+        result = Oj.dump(transformed_object, mode: :compat)
 
-      env['rack.input'] = StringIO.new(result)
-      # Rails uses this elsewhere to parse 'rack.input', it must be updated to avoid truncation
-      env['CONTENT_LENGTH'] = result.length.to_s
+        env['rack.input'] = StringIO.new(result)
+        # Rails uses this elsewhere to parse 'rack.input', it must be updated to avoid truncation
+        env['CONTENT_LENGTH'] = result.length.to_s
+      end
 
       @app.call(env)
     end
--- lib/json_key_transformer_middleware/railtie.rb
+++ lib/json_key_transformer_middleware/railtie.rb
@@ -9,6 +9,7 @@ module JsonKeyTransformerMiddleware
     config.json_key_transformer_middleware.incoming_strategy_options = ActiveSupport::OrderedOptions.new
     config.json_key_transformer_middleware.outgoing_strategy = :transform_underscore_to_camel
     config.json_key_transformer_middleware.outgoing_strategy_options = ActiveSupport::OrderedOptions.new
+    config.json_key_transformer_middleware.skip_paths = []
 
     config.app_middleware.insert_after(Rails::Rack::Logger, JsonKeyTransformerMiddleware::IncomingParamsFormatter, config.json_key_transformer_middleware)
     config.app_middleware.insert_after(Rails::Rack::Logger, JsonKeyTransformerMiddleware::IncomingJsonFormatter, config.json_key_transformer_middleware)
